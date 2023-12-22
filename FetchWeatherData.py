b2a09f1618571ce1ceeeb5d16f8c5529b4972453
elibrade <151529586+elibrade@users.noreply.github.com>
2023-12-22 19:56:12 +0200

    Python script fetching FMI temperature data
    The script with location parameter is called from MessageReceived.scala

---
+++ FetchWeatherData.py
@@ -0,0 +1,22 @@
+import fmi_weather_client as fmi
+from fmi_weather_client.errors import ClientError, ServerError
+import sys
+
+def get_weather_report(location):
+    try:
+        forecast = fmi.forecast_by_place_name(location)
+        return f"{forecast.place}: {forecast.forecasts[2].temperature.value} C"
+
+    except ClientError as err:
+        # Catch and return client errors (invalid coordinate, unknown place, etc.)
+        return f"FMI returned a client error {err.status_code}: {err.message}"
+
+    except ServerError as err:
+        # Catch and return server errors
+        return f"FMI returned a server error {err.status_code}: {err.body}"
+
+if __name__ == '__main__':
+    location = sys.argv[1]
+    result = get_weather_report(location)
+    print(result)
+    result
