9d7ddb0adc6779d377e6c7977c9fd998f5f65358
elibrade <151529586+elibrade@users.noreply.github.com>
2023-12-22 20:03:31 +0200

    Update Main.scala
    API key hidden

--- src/main/scala/weatherapp/Main.scala
+++ src/main/scala/weatherapp/Main.scala
@@ -4,7 +4,7 @@ import net.dv8tion.jda.api.requests.GatewayIntent
 
 
 @main def main() =
-    val builder = JDABuilder.createDefault("MTE4NDYyODc1Mjk3ODgyNTI1OQ.GT7y5i.JUTu9HdGlkR0jDE5eFxq9QCtuHZTya6BI8d07c")
+    val builder = JDABuilder.createDefault("YOUR_API_KEY")
     builder.enableIntents(GatewayIntent.MESSAGE_CONTENT)
     builder.addEventListeners(new MessageReceived)
     builder.build()
