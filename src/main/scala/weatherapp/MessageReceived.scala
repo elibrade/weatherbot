818d8a35822bd2a30fade578e001b9fc7903b019
elibrade <151529586+elibrade@users.noreply.github.com>
2024-01-07 04:32:13 +0200

    Update MessageReceived.scala
    starting work on forecast feature

--- src/main/scala/weatherapp/MessageReceived.scala
+++ src/main/scala/weatherapp/MessageReceived.scala
@@ -19,18 +19,20 @@ class MessageReceived extends ListenerAdapter:
   def executeAction(message: String): String =
     val commandText = message.trim.toLowerCase
     val verb        = commandText.takeWhile( _ != ' ' )
-    val modifiers   = commandText.drop(verb.length).trim
+    val modifiers   = commandText.drop(verb.length).trim.split(' ')
+    val location    = modifiers(0)
     val action: String =
       verb match
-        case "!weather"  => weatherReport(modifiers)
+        case "!weather"  => makeWeatherReport(location)
+        case "!forecast" => makeForecast(location)
         case other       => "Invalid command."
-    action
+    s">>> $action"
 
 
-  def weatherReport(modifiers: String): String =
+  def makeWeatherReport(location: String): String =
     try
-      val path: String = "PATH_TO_FETCHWEATHERDATA.PY"
-      val builder: ProcessBuilder = new ProcessBuilder("python", path, modifiers)
+      val path: String = "PATH TO fetchweatherdata/weatherreport.py"
+      val builder: ProcessBuilder = new ProcessBuilder("python", path, location)
       val process: Process = builder.start()
 
       val reader: BufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream))
@@ -42,13 +44,32 @@ class MessageReceived extends ListenerAdapter:
         output.toString()
       else
         throw new RuntimeException("Error executing Python script. Exit code: " + exitCode)
+    catch
+      case e: Exception =>
+        s"Error fetching weather information: ${e.getMessage}"
 
+
+  def makeForecast(location: String): String =
+    try
+      val path: String = "PATH TO fetchweatherdata/forecast.py"
+      val builder: ProcessBuilder = new ProcessBuilder("python", path, location)
+      val process: Process = builder.start()
+
+      val reader: BufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream))
+      val output: StringBuilder = new StringBuilder()
+      output.append(reader.readLine())
+
+      val exitCode: Int = process.waitFor()
+      if exitCode == 0 then
+        output.toString()
+      else
+        throw new RuntimeException("Error executing Python script. Exit code: " + exitCode)
     catch
       case e: Exception =>
         s"Error fetching weather information: ${e.getMessage}"
 
 
 
-
 end MessageReceived
+
 
